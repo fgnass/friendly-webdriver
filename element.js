77d18abe55a88d595f95dc2d2d6aab7a1933272c
Felix Gnass <fgnass@gmail.com>
2016-02-17 16:30:12 +0100

    require selenium-webdriver directly

--- element.js
+++ element.js
@@ -1,62 +1,58 @@
 var assign = require('object-assign');
 var webdriver = require('selenium-webdriver');
 
-module.exports = function () {
+var fn = {
+  attr: function (name) {
+    return this.getAttribute(name);
+  },
 
-  var fn = {
-    attr: function (name) {
-      return this.getAttribute(name);
-    },
+  css: function (prop) {
+    return this.getCssValue(prop);
+  },
 
-    css: function (prop) {
-      return this.getCssValue(prop);
-    },
+  find: function (sel) {
+    return element(this.findElement({ css: sel }));
+  },
 
-    find: function (sel) {
-      return element(this.findElement({ css: sel }));
-    },
+  findAll: function (sel) {
+    return this.findElements({ css: sel }).map(element);
+  },
 
-    findAll: function (sel) {
-      return this.findElements({ css: sel }).map(element);
-    },
-
-    /**
-     * el.press('ctrl+a', 'ctrl+x')
-     */
-    press: function (/* chord1, chord2, ... */) {
-      var args = Array.prototype.slice.call(arguments);
-      var keys = args.map(function (chord) {
-        var keys = chord.split(/[+-](?!$)/).map(function (key) {
-          return webdriver.Key[key.toUpperCase()] || key;
-        });
-        if (keys.length) {
-          return webdriver.Key.chord.apply(webdriver.Key, keys);
-        }
-        return keys[0];
+  /**
+   * el.press('ctrl+a', 'ctrl+x')
+   */
+  press: function (/* chord1, chord2, ... */) {
+    var args = Array.prototype.slice.call(arguments);
+    var keys = args.map(function (chord) {
+      var keys = chord.split(/[+-](?!$)/).map(function (key) {
+        return webdriver.Key[key.toUpperCase()] || key;
       });
-      return this.sendKeys.apply(this, keys);
-    },
-
-    fill: function (attr, values) {
-      if (arguments.length == 1) {
-        if (typeof attr == 'string') {
-          this.clear();
-          return this.sendKeys(attr);
-        }
-        values = attr;
-        attr = 'name';
+      if (keys.length) {
+        return webdriver.Key.chord.apply(webdriver.Key, keys);
       }
-      var self = this;
-      Object.keys(values).forEach(function (name) {
-        self.find('[' + attr + '=' + name + ']').fill(values[name]);
-      });
+      return keys[0];
+    });
+    return this.sendKeys.apply(this, keys);
+  },
+
+  fill: function (attr, values) {
+    if (arguments.length == 1) {
+      if (typeof attr == 'string') {
+        this.clear();
+        return this.sendKeys(attr);
+      }
+      values = attr;
+      attr = 'name';
     }
-  };
-
-  function element(el) {
-    return assign(Object.create(el), fn);
+    var self = this;
+    Object.keys(values).forEach(function (name) {
+      self.find('[' + attr + '=' + name + ']').fill(values[name]);
+    });
   }
-
-  return element;
-
 };
+
+function element(el) {
+  return assign(Object.create(el), fn);
+}
+
+module.exports = element;
--- until.js
+++ until.js
@@ -1,79 +1,77 @@
 var URL = require('url');
+var webdriver = require('selenium-webdriver');
 
-module.exports = function (webdriver) {
+var until = webdriver.until;
+var Condition = until.Condition;
 
-  var until = webdriver.until;
-  var Condition = until.Condition;
+function all(conds, method) {
+  if (conds.length == 1) {
+    return conds[0];
+  }
+  if (!method) method = 'every';
 
-  function all(conds, method) {
-    if (conds.length == 1) {
-      return conds[0];
-    }
-    if (!method) method = 'every';
+  var descriptions = conds.map(function (c) {
+    return c.description();
+  });
 
-    var descriptions = conds.map(function (c) {
-      return c.description();
+  var desc = ['for ' + method + ' of these:'].concat(descriptions).join('\n* ');
+
+  return new Condition(desc, function (driver) {
+    return webdriver.promise.all(conds.map(function (cond) {
+      return cond.fn(driver);
+    }))
+    .then(function (values) {
+      return values[method](Boolean);
     });
+  });
+}
 
-    var desc = ['for ' + method + ' of these:'].concat(descriptions).join('\n* ');
+var builders = {
 
-    return new Condition(desc, function (driver) {
-      return webdriver.promise.all(conds.map(function (cond) {
-        return cond.fn(driver);
-      }))
-      .then(function (values) {
-        return values[method](Boolean);
+  css: function (sel) {
+    return until.elementLocated({ css: sel });
+  },
+
+  url: function (url) {
+    return new Condition('for URL to become ' + url, function (driver) {
+      return driver.getCurrentUrl().then(function (current) {
+        return URL.resolve(current, url) == current;
+      });
+    });
+  },
+
+  title: function (title) {
+    if (title instanceof RegExp) return until.titleMatches(title);
+    return until.titles(title);
+  },
+
+  unless: function (spec) {
+    var cond = build(spec);
+    return new Condition('unless ' + spec, function (driver) {
+      return new webdriver.promise.Promise(function (res, rej) {
+        cond.fn(driver).then(function (value) {
+          var err = new Error(cond.description());
+          err.cause = value;
+          err.fn = cond.fn;
+          rej(err);
+        });
       });
     });
   }
-
-  var builders = {
-
-    css: function (sel) {
-      return until.elementLocated({ css: sel });
-    },
-
-    url: function (url) {
-      return new Condition('for URL to become ' + url, function (driver) {
-        return driver.getCurrentUrl().then(function (current) {
-          return URL.resolve(current, url) == current;
-        });
-      });
-    },
-
-    title: function (title) {
-      if (title instanceof RegExp) return until.titleMatches(title);
-      return until.titles(title);
-    },
-
-    unless: function (spec) {
-      var cond = build(spec);
-      return new Condition('unless ' + spec, function (driver) {
-        return new webdriver.promise.Promise(function (res, rej) {
-          cond.fn(driver).then(function (value) {
-            var err = new Error(cond.description());
-            err.cause = value;
-            err.fn = cond.fn;
-            rej(err);
-          });
-        });
-      });
-    }
-  };
-
-  function build(spec) {
-    // if an array is given wait until ANY condition is satisfied
-    if (Array.isArray(spec)) return all(spec.map(build), 'some');
-
-    // default to css
-    if (typeof spec == 'string') spec = { css: spec };
-
-    // if an object is given, wait until ALL props are satisfied
-    return all(Object.keys(spec).map(function (name) {
-      if (!builders[name]) throw Error('no such condition:' + name);
-      return builders[name](spec[name]);
-    }));
-  }
-
-  return build;
 };
+
+function build(spec) {
+  // if an array is given wait until ANY condition is satisfied
+  if (Array.isArray(spec)) return all(spec.map(build), 'some');
+
+  // default to css
+  if (typeof spec == 'string') spec = { css: spec };
+
+  // if an object is given, wait until ALL props are satisfied
+  return all(Object.keys(spec).map(function (name) {
+    if (!builders[name]) throw Error('no such condition:' + name);
+    return builders[name](spec[name]);
+  }));
+}
+
+module.exports = build;
